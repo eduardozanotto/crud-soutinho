efd58f9990b31f204da571ee3fe1811eeb987d64
Eduardo Zanotto <eduardozanotto@mochilas-mbp.broadband>
2024-07-20 23:16:09 +0100

    started eslint and prettier

---
+++ core/crud.ts
@@ -0,0 +1,115 @@
+import fs from "fs";
+import { v4 as uuid } from "uuid";
+const DB_FILE_PATH = "./core/db";
+
+console.log("CRUD");
+
+type UUID = string;
+
+interface Todo {
+  id: UUID;
+  date: string;
+  content: string;
+  done: boolean;
+}
+
+function create(content: string): Todo {
+  const todo: Todo = {
+    id: uuid(),
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
+
+  const todos: Array<Todo> = [...read(), todo];
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2));
+  return todo;
+}
+
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+
+  if (!db.todos) {
+    return [];
+  }
+
+  return db.todos;
+}
+
+function update(id: UUID, partilTodo: Partial<Todo>) {
+  let updatedTodo;
+  const todos = read();
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id === id;
+    if (isToUpdate) {
+      updatedTodo = Object.assign(currentTodo, partilTodo);
+    }
+  });
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
+
+  if (!updatedTodo) {
+    throw new Error("Please provide another todo");
+  }
+
+  return updatedTodo;
+}
+
+function updateContentById(id: UUID, content: string) {
+  return update(id, {
+    content,
+  });
+}
+
+function deleteByTodo(id: UUID) {
+  const todos = read();
+
+  const todosWithoutOne = todos.filter((todo) => {
+    if (id === todo.id) {
+      return false;
+    }
+    return true;
+  });
+
+  console.log("TODOS", todos);
+  console.log("WITHOUT", todosWithoutOne);
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos: todosWithoutOne,
+      },
+      null,
+      2
+    )
+  );
+}
+
+function CLEAR_DB() {
+  fs.writeFileSync(DB_FILE_PATH, "");
+}
+
+CLEAR_DB();
+
+create("primeira TODO");
+
+const secondTodo = create("segunda TODO");
+
+deleteByTodo(secondTodo.id);
+
+const terceiraTodo = create("segunda TODO");
+
+updateContentById(terceiraTodo.id, "aaaaa");
+
+console.log("CRUD", read());
